3e3c12a190b203daba83d95439b3673e3358dd75
Courtney Morrissey <courtneycmorrissey@gmail.com>
2016-05-12 13:24:08 -0600

    users signup

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,58 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const knex = require('../db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 /* GET users listing. */
 router.post('/signup', function(req, res, next) {
   const errors = []
 
- if (!req.body.email || !req.body.email.trim()) errors.push("Email can't be blank");
- if (!req.body.name || !req.body.name.trim()) errors.push("Name can't be blank");
- if (!req.body.password || !req.body.password.trim()) errors.push("Password can't be blank");
+  if (!req.body.email || !req.body.email.trim()) errors.push("Email can't be blank");
+  if (!req.body.name || !req.body.name.trim()) errors.push("Name can't be blank");
+  if (!req.body.password || !req.body.password.trim()) errors.push("Password can't be blank");
 
- if (errors.length) {
-   res.status(422).json({
-     errors: errors
-   })
- }
+  if (errors.length) {
+    res.status(422).json({
+      errors: errors
+    })
+  } else {
+    knex('users')
+      .whereRaw('lower(email) = ?', req.body.email.toLowerCase())
+      .count()
+      .first()
+      .then(function (result) {
+        //{count: "0"}
+        if (result.count ==="0"){
+          const saltRounds = 4;
+          const passwordHash = bcrypt.hashSync(req.body.password, saltRounds);
+
+            knex('users')
+            .insert({
+              email: req.body.email,
+              name: req.body.name,
+              password_hash: passwordHash
+            })
+            .returning('*')
+            .then(function (users){
+              const user = users[0];
+              const token = jwt.sign({id: user.id}, process.env.JWT_SECRET);
+              res.json({
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                token: token
+              })
+            })
+
+        } else {
+          res.status(422).json({
+            errors: ["Email has already been taken"]
+          })
+        }
+
+      })
+  }
 
 });
 
---
+++ seeds/01-users.js
@@ -0,0 +1,15 @@
+
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('users').del(),
+
+    // Inserts seed entries
+    knex('users').insert({
+      email: 'user1@example.com',
+      name: 'Dwayne Johnson',
+      password_hash: 'password'
+    })
+
+  );
+};
