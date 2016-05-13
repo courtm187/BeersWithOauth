c906739edc99971b389e32636119c66c8f039b40
Courtney Morrissey <courtneycmorrissey@gmail.com>
2016-05-13 14:29:25 -0600

    more token stuff

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10,34 +10,23 @@
   // cleanup the code a bit
 
   angular.module('app', ['ngRoute'])
-    .config(function($routeProvider, $locationProvider) {
+    .config(function($routeProvider, $locationProvider, $httpProvider) {
+      $httpProvider.interceptors.push("authInterceptor");
+
       $routeProvider
        .when('/', {
         templateUrl: '/templates/home.html',
         controller: 'HomeController',
         resolve: {
           currentUser: function ($http) {
-
-            // if the browser has a token
-            if (localStorage.getItem('token')) {
-
-              // call /me endpoint, and pass it the token
-              const config = {
-                headers: {
-                  'Authorization': 'Bearer ' + localStorage.getItem('token')
-                }
-              }
-
-              return $http.get('/api/v1/users/me', config)
-                .then(function (response) {
-                  return response.data
-                })
-                .catch(function () {
-                  localStorage.clear();
-                  return null;
-                })
-
-            }
+            return $http.get('/api/v1/users/me')
+              .then(function (response) {
+                return response.data
+              })
+              .catch(function () {
+                localStorage.clear();
+                return null;
+              })
           }
         }
       })
@@ -51,28 +40,15 @@
         requiresLogin: true, // I can put arbitrary data on a route
         resolve: {
           currentUser: function ($http, $location) {
-
-            // if the browser has a token
-            if (localStorage.getItem('token')) {
-
-              // call /me endpoint, and pass it the token
-              const config = {
-                headers: {
-                  'Authorization': 'Bearer ' + localStorage.getItem('token')
-                }
-              }
-
-              return $http.get('/api/v1/users/me', config)
-                .then(function (response) {
-                  return response.data
-                })
-                .catch(function () {
-                  localStorage.clear();
-                  $location.path("/")
-                  return null;
-                })
-
-            }
+            return $http.get('/api/v1/users/me')
+              .then(function (response) {
+                return response.data
+              })
+              .catch(function () {
+                localStorage.clear();
+                $location.path("/")
+                return null;
+              })
           }
         }
       });
@@ -87,12 +63,33 @@
         // if the next route requires login
         // and we don't have a token
         // then redirect to the homepage
-
-        if (next.$$route.requiresLogin && !localStorage.getItem('token')) {
-          $location.path('/');
-        }
+        //
+        // if (next.$$route.requiresLogin && !localStorage.getItem('token')) {
+        //   $location.path('/');
+        // }
 
       });
     });
+
+    angular.module('app').factory('authInterceptor', function ($location) {
+      return {
+        request: function(config) {
+          if (localStorage.getItem('token')) {
+            config.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
+          }
+
+          return config;
+        },
+
+        responseError: function(response) {
+
+          if (response.status === 403){
+            // localStorage.removeItem('token');
+            $location.path('/');
+          }
+          return response;
+        }
+      };
+    })
 
 }());
